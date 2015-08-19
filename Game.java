bddd1ccf0af2c66a8b77d902c5753d711f6385a3
Josh Tan <jtan189@gmail.com>
2015-08-19 10:29:06 -0400

    It's better to scope variables to the smallest scope possible.

--- Game.java
+++ Game.java
@@ -1,14 +1,14 @@
 public class Game {
 
-    static Move yourMove;
-    static Move opponentMove;
-
     public void playGame() {
+
 	Player yourPlayer = new Player();
         Opponent yourOpponent = new Opponent();
-	yourMove = yourPlayer.play();
-	opponentMove = yourOpponent.play();
+
+	Move yourMove = yourPlayer.play();
+	Move opponentMove = yourOpponent.play();
 	System.out.println("You chose " + yourMove + " and your opponent chose " + opponentMove);
+
 	if (yourMove == opponentMove) {
 	    System.out.println("You both answered " + yourMove + " please try again!");
 	} else if (yourMove == Move.ROCK && opponentMove == Move.SCISSORS) {
